c9c77fd1d1c30f6e5dcd6ed213bed67939137399
pavan <pavan2you@gmail.com>
2019-04-06 12:10:36 +0530

    ios - fixed pod file for runner

---
+++ example/ios/Runner/HelloSwift.swift
@@ -0,0 +1,16 @@
+//
+//  HelloSwift.swift
+//  Runner
+//
+//  Created by pavan on 29/03/19.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
+
+class HelloSwift : NSObject {
+    
+    func sayHello() {
+        print("Hello")
+    }
+}
